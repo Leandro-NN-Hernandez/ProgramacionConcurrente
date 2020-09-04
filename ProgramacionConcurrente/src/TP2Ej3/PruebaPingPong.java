c5f6e0033ac9b2640fafae969165f55c34341d0b
Leandro Hernandez <Leandro Hernandez@192.168.0.17>
2020-09-04 14:44:53 -0300

    Se agregaron comentarios con las respuestas de los incisos del tp2, ejercicio 3

--- ProgramacionConcurrente/src/TP2Ej3/PruebaPingPong.java
+++ ProgramacionConcurrente/src/TP2Ej3/PruebaPingPong.java
@@ -12,13 +12,34 @@ package TP2Ej3;
 public class PruebaPingPong {
 
     public static void main(String[] args) {
-        PingPong t1 = new PingPong("PING", 33);
+        PingPong t1 = new PingPong("PING", 13);
         PingPong t2 = new PingPong("PONG", 10);// Activación
+        //3.f No se puede determinar que proceso se esta ejecutando
+        PingPong t3 = new PingPong("PANG", 14);
+        PingPong t4 = new PingPong("PUNG", 12);
         t1.start();
         t2.start();// Espera unos segundos
+        t3.start();
+        t4.start();
+        // 3.c
+        for (int i = 0; i < 20; i++) {
+            System.out.println("Una sentencia cualquiera " + i);
+            /*
+            //3.d 
+            try {                
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+            }
+             */
+        }
         try {
+            //Con la siguiente sentencia, el programa tarda 5 segundos mas
             Thread.sleep(5000);
         } catch (InterruptedException e) {
         }
-    }// Finaliza la ejecución de los threads... }
+
+    }// Finaliza la ejecución de los threads... 
+    //3.e el agregar un sleep en una estructura repetitiva aumenta el tiempo
+    //de ejecuccion considerablemente y que en el programa no hay un orden
+    //de ejecucion, hay indeterministo
 }
