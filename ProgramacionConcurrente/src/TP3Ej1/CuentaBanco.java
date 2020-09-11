5d758d66d180463b52a60bd85940eb919eed9506
Leandro Hernandez <Leandro Hernandez@192.168.0.17>
2020-09-11 17:57:11 -0300

    Se realizan correcciones en TP3Ej1 Se mueve el metodo HacerRetiro(int cantidad) a CuentaBanco y se mueve el sleep al run() y se modifica retiroBancario(int retiro) a privado

--- ProgramacionConcurrente/src/TP3Ej1/CuentaBanco.java
+++ ProgramacionConcurrente/src/TP3Ej1/CuentaBanco.java
@@ -16,11 +16,27 @@ public class CuentaBanco {
     public CuentaBanco() {
     }
 
-    public  int getBalance() {
+    public synchronized int getBalance() {
         return balance;
     }
 
-    public  void retiroBancario(int retiro) {
+    private void retiroBancario(int retiro) {
         balance = balance - retiro;
     }
+
+    public synchronized void HacerRetiro(int cantidad) throws InterruptedException {
+
+        if (this.getBalance() >= cantidad) {
+            System.out.println(Thread.currentThread().getName() + " está realizando un retiro de: " + cantidad + ".");
+
+            this.retiroBancario(cantidad);
+            System.out.println(Thread.currentThread().getName() + ":Retiro realizado. ");
+            System.out.println(Thread.currentThread().getName() + ": Los fondos son de: " + this.getBalance());
+        } else {
+            System.out.println("No hay suficiente dinero en la cuenta para realizar el retiro Sr." + Thread.currentThread().getName()
+            );
+            System.out.println("Su saldo actual es de " + this.getBalance());
+            Thread.sleep(1000);
+        }
+    } // de hacer retiro
 }
